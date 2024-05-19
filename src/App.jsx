6d2d12701281b577f19cbe499bbd7af880470013
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-05-19 08:59:44 +0530

    Data.jsx: results displayed through map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import Header from './components/Header.jsx';
+import {React,useState} from 'react';
+//import Header from './components/Header.jsx';
 import Data from './components/Data.jsx'
-import {calculateInvestmentResults,formatter} from './util/investment.js';
+
+let  initialInvestment= 1000;
+let annualInvestment= 5000;
+let expectedReturn=500000;
+let duration= 10;
+/*
+console.log(calculateInvestmentResults(data));*/
+
 
 
 function App() {
+
+  const [initialValue,setValue]=useState({  initialInvestment: 1000,
+    annualInvestment: 5000,
+    expectedReturn:500000,
+    duration: 10,});
+
+    function calculateInvestment(e)
+    {
+      const {name,value}=e.target;
+      setValue({...initialValue,[name]:value});
+      //console.log(target);
+      //console.log(value);
+      //console.log(text);
+      console.log(initialValue);
+
+    }
+
   return (
 
     <>
+
+    {/*
+  <Header id="user-input" className="input-group" onChange={calculateInvestment} value={initialValue}/>
+  */}
+
+  <header id="user-input" >
+  <div >
+
+
+<label htmlFor="initial investment">INITIAL INVESTMENT</label>
+
+<input type="number" className="input-group"  name="initialInvestment" value={initialValue.initialInvestment} required onChange={calculateInvestment}/>
+
+<label htmlFor="annual investment">ANNUAL INVESTMENT</label>
+
+<input type="number" className="input-group" name="annualInvestment" value={initialValue.annualInvestment} required onChange={calculateInvestment}/>
+
+<label htmlFor="expected return">EXPECTED RETURN</label>
+
+<input type="number" className="input-group" name="expectedReturn" value={initialValue.expectedReturn} required onChange={calculateInvestment}/>
+
+<label htmlFor="duration">DURATION</label>
+
+<input type="number" className="input-group" name="duration" value={initialValue.duration} required onChange={calculateInvestment}/>
+</div>
+  </header>
     
-    <Header id="user-input" className="input-group"/>
-    <Data id='result'/>
-    
+  <Data id='result' value={initialValue} />
     </>
   )
 }
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import {calculateInvestmentResults,formatter} from '../util/investment.js';
+// year, Investment Value, Interest(Year), Total interest, Invested capital
+export default function Data({id,value}){
 
-export default function Data({id}){
+
+
+    let content=calculateInvestmentResults(value);
+    console.log(typeof content);
+    if (!Array.isArray(content)) {
+        console.log('calculateInvestmentResults did not return an array:');
+        content = [];
+      }
+
     return (
 
-        <div id={id}>
-            <table>
+        <div >
+            <table id={id}>
                 <thead>
-                    data
+                    <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest(Year)</th>
+                    <th>Total interest</th>
+                    <th> Invested capital</th>
+                    </tr>
+                        
                 </thead>
                 <tbody>
-                    data
+                {content.map((data,index)=>(<tr key={index}>
+
+              <td>{data.year}</td>
+              <td>{formatter.format(data.valueEndOfYear)}</td>
+              <td>{formatter.format(data.interest)}</td>
+              <td>{formatter.format(data.annualInvestment)}</td>
+              <td>{formatter.format(data.annualInvestment)}</td>
+                                
+
+                </tr>))}
                 </tbody>
             </table>
         </div>
